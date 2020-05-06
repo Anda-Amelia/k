f39d7407cc193a810c648bd840cd75dc66c78693
Denis Bogdanas <denis.bogdanas@gmail.com>
2020-05-06 17:46:37 +0300

    CheckFunctions: revert changes

--- kernel/src/main/java/org/kframework/compile/checks/CheckFunctions.java
+++ kernel/src/main/java/org/kframework/compile/checks/CheckFunctions.java
@@ -72,9 +72,7 @@ public class CheckFunctions {
                       || hook.equals("SET.unit") || hook.equals("LIST.element")
                       || hook.equals("LIST.concat") || hook.equals("LIST.unit")
                       || hook.equals("MAP.element") || hook.equals("MAP.concat")
-                      || hook.equals("MAP.unit")
-                        //Required for #Ceil rules
-                      || k.klabel().name().equals("padRightBytes") || k.klabel().name().equals("padLeftBytes"))) {
+                      || hook.equals("MAP.unit"))) {
                   errors.add(KEMException.compilerError("Illegal function symbol on LHS of rule.", k));
                 }
                 atTop = false;
